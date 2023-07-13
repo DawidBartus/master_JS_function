c96e7348ea012f85cf0ec643f9f2dcf4e9d495b3
Dawid <109389692+DawidBartus@users.noreply.github.com>
2023-07-13 20:58:18 +0200

    funny background

---
+++ play.js
@@ -0,0 +1,5 @@
+const background = document.getElementById("background");
+console.log(background);
+const purpleDot = document.getElementById("purpleDot");
+// const style =
+// purpleDot.style.add("")
